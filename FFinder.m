8a5c96b34dcbaf858583d401043f2886e72513ef
schasen1005 <71855903+schasen1005@users.noreply.github.com>
2020-12-04 15:58:21 -0700

    Add files via upload

---
+++ FFinder.m
@@ -0,0 +1,16 @@
+function [F] = FFinder(Fqpos,Fqneg,pd)
+for j=1:length(p)
+    qd(j) = sym( sprintf('qd%d', j), 'real' );
+end
+for j=1:length(p)
+    if pd(i)<0
+        Fq(j)=subs(Fqneg(j),qd(j),p(j));
+    elseif pd(i)>0
+        Fq(j)=subs(Fqpos(j),qd(j),p(j));
+    else
+        Fq(j)=0;
+    end
+end
+F=eval(Fq);
+end
+
