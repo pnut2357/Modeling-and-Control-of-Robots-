71cb004e9bf18abb848723c6d71448ac3bd1e90e
Ani9691 <71851678+Ani9691@users.noreply.github.com>
2020-12-04 15:48:33 -0700

    Add files via upload

--- CFinder.m
+++ CFinder.m
@@ -2,8 +2,10 @@ function [C] = CFinder(Cq, p, dp)
 % Getting C
 for j=1:length(p)
     q(j) = sym( sprintf('q%d', j), 'real' );
+    qd(j) = sym( sprintf('qd%d', j), 'real' );
 end
 Cq=subs(Cq,q,p);
+Cq=subs(Cq,qd,dp);
 C=eval(Cq);
 end
 
